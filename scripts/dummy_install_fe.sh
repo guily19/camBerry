0c7c5bd2adbd952316052df79d7c5e7c3e27146c
albertomarin <alberto@localhost.localdomain>
2015-11-09 13:59:38 +0100

    Updated path

--- scripts/dummy_install_fe.sh
+++ scripts/dummy_install_fe.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-cp -rp 'web_portal_FE/view/*' '/var/www/html/'
-cp -rp 'web_portal_FE/js/*' '/var/www/html/js/'
-cp -rp 'web_portal_FE/img/*' '/var/www/html/img/'
-cp -rp 'web_portal_FE/icons/*' '/var/www/html/icons/'
-cp -rp 'web_portal_FE/icons/*' '/var/www/html/icons/'
-cp -rp 'web_portal_FE/css/*' '/var/www/html/css/'
+cp -rp '../web_portal_FE/view/*' '/var/www/html/'
+cp -rp '../web_portal_FE/js/*' '/var/www/html/js/'
+cp -rp '../web_portal_FE/img/*' '/var/www/html/img/'
+cp -rp '../web_portal_FE/icons/*' '/var/www/html/icons/'
+cp -rp '../web_portal_FE/icons/*' '/var/www/html/icons/'
+cp -rp '../web_portal_FE/css/*' '/var/www/html/css/'
 
 chmod -R 655 '/var/www/html/'
 
