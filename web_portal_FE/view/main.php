3a27b57f6253146351ef74faf4dbeacc5cfedf16
guillem.casanova <guillemcasanova@gmail.com>
2015-11-09 15:30:11 +0100

    Fix error with main.php

--- web_portal_FE/view/main.php
+++ web_portal_FE/view/main.php
@@ -3,58 +3,6 @@ $conn = mysql_connect("localhost","root","S539CW7LjXk_v7A");
 
 mysql_select_db("CamberryDB",$conn); 
 
-//primero tengo que ver si el usuario está memorizado en una cookie
-if (isset($_COOKIE["id_usuario_dw"]) && isset($_COOKIE["marca_aleatoria_usuario_dw"])){
-   //Tengo cookies memorizadas
-   //además voy a comprobar que esas variables no estén vacías
-   if ($_COOKIE["id_usuario_dw"]!="" || $_COOKIE["marca_aleatoria_usuario_dw"]!=""){
-      //Voy a ver si corresponden con algún usuario
-      $ssql = "select * from Users where USER='" . $_COOKIE["id_usuario_dw"] . "' and COOKIE='" . $_COOKIE["marca_aleatoria_usuario_dw"] . "'";
-      $rs = mysql_query($ssql);
-      if (mysql_num_rows($rs)==1){
-         echo "<b>Tengo un usuario correcto en una cookie</b>";
-         $usuario_encontrado = mysql_fetch_object($rs);
-         echo "<br>Eres el usuario número " . $usuario_encontrado->USER . ", de nombre " . $usuario_encontrado->usuario;
-         header ("Location: main.html");
-      }
-   }
-}
-
-if ($_GET){
-   //estan intentant accedir directament al ficher main.php
-
-   //debería comprobar si el usuario es correcto
-   $ssql = "select * from Users where MAIL = '" . $_POST["Email"] . "' and PASSWORD='" . $_POST["password"] . "'";
-   //echo $ssql;
-   $rs = mysql_query($ssql);
-   if (mysql_num_rows($rs)==1){
-      //TODO CORRECTO!! He detectado un usuario
-      $usuario_encontrado = mysql_fetch_object($rs);
-      //ahora debo de ver si el usuario quería memorizar su cuenta en este ordenador
-    
-         //es que pidió memorizar el usuario
-         //1) creo una marca aleatoria en el registro de este usuario
-         //alimentamos el generador de aleatorios
-         mt_srand (time());
-         //generamos un número aleatorio
-         $numero_aleatorio = mt_rand(1000000,999999999);
-         //2) meto la marca aleatoria en la tabla de usuario
-         $ssql = "update Users set COOKIE='$numero_aleatorio' where MAIL='" . $_POST["Email"] . "'";
-         mysql_query($ssql);
-         //3) ahora meto una cookie en el ordenador del usuario con el identificador del usuario y la cookie aleatoria
-         setcookie("id_usuario_dw", $usuario_encontrado->USER , time()+(60*60*24*365));
-         setcookie("marca_aleatoria_usuario_dw", $numero_aleatorio, time()+(60*60*24*365));
-     
-      echo "Autenticado correctamente";
-      header ("Location: main.html");
-      
-   }else{
-      echo "Fallo de autenticación!";
-      echo "<p><a href='index.php'>Volver</a>";
-      echo "<p>$rs es la sortida de query, $ssql la consulta, connexio $conn";
-   }
-   
-}else{
 ?>
 
 <!DOCTYPE html>
