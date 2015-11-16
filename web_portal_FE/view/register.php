a5705be87b3992e3fc2c37854995eb4ca71c8ea2
albertomarin <alberto@localhost.localdomain>
2015-11-16 16:32:27 +0100

    Fixed more bugs on register.php

--- web_portal_FE/view/register.php
+++ web_portal_FE/view/register.php
@@ -93,19 +93,19 @@ include('db_access.php');
                 <div class="form-group">
                     <label for="inputEmail3" class="col-sm-2 control-label">Email:</label>
                     <div class="col-sm-10">
-                        <input type="email" class="input form-control" name="user "id="inputEmail3" placeholder="Email">
+                        <input type="email" class="input form-control" name="email"id="inputEmail3" placeholder="Email">
                     </div>
                 </div>
                 <div class="form-group">
                     <label for="inputUser3" class="col-sm-2 control-label">Password:</label>
                     <div class="col-sm-10">
-                        <input type="password" class="input form-control" name="user "id="inputPassword3" placeholder="Password">
+                        <input type="password" class="input form-control" name="pwd"id="inputPassword3" placeholder="Password">
                     </div>
                 </div>
                 <div class="form-group">
                     <label for="inputUser3" class="col-sm-2 control-label">Repeat Password:</label>
                     <div class="col-sm-10">
-                        <input type="password" class="input form-control" name="user "id="inputPassword3" placeholder="Password">
+                        <input type="password" class="input form-control" name="pwd2"id="inputPassword3" placeholder="Password">
                     </div>
                 </div>
     <!-- 		        First name:<br>
