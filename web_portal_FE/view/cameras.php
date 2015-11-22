ed658f0ce35be2baa181dfe202f82c094bbc494f
guillem.casanova <guillemcasanova@gmail.com>
2015-11-22 16:09:21 +0100

    Prova response php

--- web_portal_FE/view/cameras.php
+++ web_portal_FE/view/cameras.php
@@ -1,6 +1,16 @@
 <!-- This file will return de cameras of an user and the public cameras depending the http req -->
 
 <?php
-	
-
+   /*
+   * Collect all Details from Angular HTTP Request.
+   */ 
+    $postdata = file_get_contents("php://input");
+    $request = json_decode($postdata);
+    // @$email = $request->email;
+    // @$pass = $request->pass;
+    echo $request;
+    // echo $email; //this will go back under "data" of angular call.
+    /*
+     * You can use $email and $pass for further work. Such as Database calls.
+    */    
 ?>
