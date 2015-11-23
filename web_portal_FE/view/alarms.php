9742db1326e46685fd36f58e0c4e8698e7740e08
guillem.casanova <guillemcasanova@gmail.com>
2015-11-23 16:14:33 +0100

    teting alarms

--- web_portal_FE/view/alarms.php
+++ web_portal_FE/view/alarms.php
@@ -16,7 +16,7 @@
 	error_log("Files->".json_encode($files));
 
 	if($files === false){
-		echo json_encode(undefined);
+		echo undefined;
 	} else {
 		//Netegem el array files perque no mostri . i ..
 		for ($i=0; $i < sizeof($files); $i++) { 
