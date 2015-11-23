200512b197d855d454001d207eaed32fce12c92c
guillem.casanova <guillemcasanova@gmail.com>
2015-11-23 17:38:40 +0100

    Settigs test

--- web_portal_FE/view/settings.php
+++ web_portal_FE/view/settings.php
@@ -1,3 +1,22 @@
 <?php
 
+	session_start();
+	include('db_access.php'); 
+	// The request is a JSON request.
+	// We must read the input.
+	// $_POST or $_GET will not work!
+
+	error_log("Settings -----------------------------------");
+
+	$data = file_get_contents("php://input");
+
+	//$username = json_decode($data);
+	if(isset($_SESSION['USER'])) {
+		$username = $_SESSION['USER'];
+  	} else {
+  		header("Location: index.php");
+  	}
+
+  	error_log("Data ->",json_decode($data));
+
 ?>
