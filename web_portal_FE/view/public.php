ad1c9145974beef6a0e4d68cf7fc53410622bedf
oscar hernan sab <oscar hernan sab>
2015-11-28 19:50:39 +0100

    making cameras public

--- web_portal_FE/view/public.php
+++ web_portal_FE/view/public.php
@@ -24,6 +24,17 @@
     $query = "UPDATE Cameras SET public = 1 WHERE id='".$id."'";
     $reg = mysql_query($query);
     }
+    $total = 0;
+    if(!empty($_POST["cams"])){
+
+      $cams = $_POST["cams"];
+      foreach ($cams as $cam){
+      $total = $total + $cam;  
+      }
+      //TODO
+      //error_log("check that the params are correct");
+      //Add camera
+      error_log("There are all params to save camera");
   	//while ($id = mysql_fetch_assoc($cameras)) {
 
   //	$query = "UPDATE Cameras SET public = 1 WHERE id='".$id."'";
@@ -32,9 +43,9 @@
      
   //	$reg = mysql_query($query);
   //}
-    echo "La camara ha sido añadida correctamente $cameras //// $id ";
+  
   		if($reg) {
-            echo "La camara ha sido añadida correctamente $query";
+            echo "La camara ha sido añadida correctamente $query /// $total";
             //header ("Location: main.php");
         }else {
             echo "ha ocurrido un error y no se guardo la camara $query";
