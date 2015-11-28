f46a5836e55e1433f6aaf9dc0f7d988ff8100099
guillem.casanova <guillemcasanova@gmail.com>
2015-11-28 11:09:36 +0100

    Fix server dont find alarms -> change to absolute path

--- web_portal_FE/view/alarms.php
+++ web_portal_FE/view/alarms.php
@@ -33,7 +33,7 @@
 		for ($i=0; $i < sizeof($files); $i++) { 
 			# code...
 			header('Content-type: image/jpg');
-			$file = $dir.$files[$i];
+			$file = $dir.'/'.$files[$i];
 			error_log($file);
 			readfile($file);
 			array_push($response, $file);
