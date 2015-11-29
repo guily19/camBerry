32525ca0d18a692aeaca2391028ab8e938a8f799
oscar hernan sab <oscar hernan sab>
2015-11-29 18:03:21 +0100

    improving public

--- web_portal_FE/view/cameras.php
+++ web_portal_FE/view/cameras.php
@@ -32,7 +32,7 @@
 
 		while($row = mysql_fetch_array($res, MYSQL_ASSOC)) {
 			$check=" ";
-			if($row['public'] == 1) $check = "checked";
+			if($row['public'] == 1) $check = checked;
 			$current_camera = array(
 				'img' => $row['img'],
 				'site' => $row['site'],
