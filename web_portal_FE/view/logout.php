ed7344b1f38b9348b1c310a2dc80775a13a8a40b
albertomarin <alberto@localhost.localdomain>
2015-11-29 18:37:24 +0100

    Fixed logoutp.php, no bd include

--- web_portal_FE/view/logout.php
+++ web_portal_FE/view/logout.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    include('db_access.php'); // incluímos los datos de acceso a la BD
+    //include('db_access.php'); // incluímos los datos de acceso a la BD
     // comprobamos que se haya iniciado la sesión
     if(isset($_SESSION['USER'])) {
         session_destroy();
