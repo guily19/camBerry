59dda4b19b97bc09d69a21c722f7def1dfe9b4c3
oscar hernan sab <oscar hernan sab>
2015-11-29 16:34:02 +0100

    improving public

--- web_portal_FE/view/main.php
+++ web_portal_FE/view/main.php
@@ -155,7 +155,7 @@
             <div ng-repeat="camera in cameras">
               <div class="form-group">
                   <label>{{camera.site}}</label>
-                  <input type="checkbox" name="cams[]" class="form-control" value = {{camera.site}} id="video" <?php if(isPublic(?>{{camera.site}}<?php)){  ?> checked  <?php }  ?> >
+                  <input type="checkbox" name="cams[]" class="form-control" value = {{camera.site}} id="video" <?php $name= ?> {{camera.site}} <?php ; if(isPublic($name)){  ?> checked  <?php }  ?> >
               </div>
             </div>
             <button type="submit" class="btn btn-default">Publicar</button>
